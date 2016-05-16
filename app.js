5327f11217ca78e603ed78f367f497aa4e9517b5
sunkay <sunkay11@gmail.com>
2016-05-15 20:28:21 -0400

    docker-compose

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+//DB setup
+//var mongourl = process.env.MONGO_URI? process.env.MONGO_URI : 'mongodb://momgo:27017/';
+//console.log("MongoURL: ", mongourl);
+mongoose.connect("mongodb://mongo:27017");
 
 app.get('/', function(req, res){
-  res.send("Hello World-changed-1");
+  res.send("Hello World-changed-1   ");
 });
 
 app.listen(3000, function(){
